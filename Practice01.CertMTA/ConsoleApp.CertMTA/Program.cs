a0ead3d4e3ff99696d5e489b478b91a256c5c2ff
Jeroo <SALva18??>
2019-04-18 01:41:03 -0400

    Initial Commit

---
+++ Practice01.CertMTA/ConsoleApp.CertMTA/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.CertMTA
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int x = 0;
+            int y = 0;
+            int resultInt = 0;
+
+            x++; // Operadores unarios
+            ++x;// Operadores unarios
+
+            resultInt = x + y; // Operadores binarios
+
+            string resultSTR = x > 0 ? "Si": "No" ; //TODO Operador Ternario, solo existe un solo operador ternario en C#
+        }
+    }
+}
