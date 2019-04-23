71ae3b60e01c0adf65a89364f54daf13e2710530
Jeroo <SALva18??>
2019-04-23 01:03:01 -0400

    58

--- Practice01.CertMTA/ConsoleApp.CertMTA/Program.cs
+++ Practice01.CertMTA/ConsoleApp.CertMTA/Program.cs
@@ -259,4 +259,23 @@ namespace ConsoleApp.CertMTA
             }
         }
     }
+
+
+    public class Rectangle
+    {
+        private double length;
+        private double width;
+
+        public Rectangle(double l, double w)
+        {
+            length = l;
+            width = w;
+        }
+
+        public double GetArea() {
+
+            return length * width;
+        }
+
+    }
 }
